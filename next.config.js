ec677992a643da0edb190886b2e9db1bc05d5b01
Rob Lloveras <Sensiblemnd@users.noreply.github.com>
2022-08-14 17:00:51 -0400

    fix

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
-const path = require("path");
+const path = require("path")
 
 module.exports = {
   reactStrictMode: true,
   sassOptions: {
     includePaths: [path.join(__dirname, "styles/sass")],
   },
-};
+  eslint: {
+    dirs: ["pages", "utils", "components", "interfaces", "lib"], // Only run ESLint on the 'pages' and 'utils' directories during production builds (next build)
+  },
+}
--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -1,29 +1,29 @@
-import ErrorPage from "next/error";
-import Head from "next/head";
-import { useRouter } from "next/router";
-import Container from "../../components/container";
-import Header from "../../components/header";
-import Layout from "../../components/layout";
-import PostBody from "../../components/post-body";
-import PostHeader from "../../components/post-header";
-import PostTitle from "../../components/post-title";
-import type PostType from "../../interfaces/post";
-import { getAllPosts, getPostBySlug } from "../../lib/api";
-import { CMS_NAME } from "../../lib/constants";
-import markdownToHtml from "../../lib/markdownToHtml";
+import ErrorPage from "next/error"
+import Head from "next/head"
+import { useRouter } from "next/router"
+import Container from "../../components/container"
+import Header from "../../components/header"
+import Layout from "../../components/layout"
+import PostBody from "../../components/post-body"
+import PostHeader from "../../components/post-header"
+import PostTitle from "../../components/post-title"
+import type PostType from "../../interfaces/post"
+import { getAllPosts, getPostBySlug } from "../../lib/api"
+import { CMS_NAME } from "../../lib/constants"
+import markdownToHtml from "../../lib/markdownToHtml"
 
 type Props = {
-  post: PostType;
-  morePosts: PostType[];
-  preview?: boolean;
-};
+  post: PostType
+  morePosts: PostType[]
+  preview?: boolean
+}
 
-export default function Post({ post, morePosts, preview }: Props) {
-  const router = useRouter();
+export default function Post({ post, preview }: Props) {
+  const router = useRouter()
   if (!router.isFallback && !post?.slug) {
-    return <ErrorPage statusCode={404} />;
+    return <ErrorPage statusCode={404} />
   }
-  const title = `${post.title} | Next.js Blog Example with ${CMS_NAME}`;
+  const title = `${post.title} | Next.js Blog Example with ${CMS_NAME}`
   return (
     <Layout preview={preview}>
       <Container>
@@ -49,14 +49,14 @@ export default function Post({ post, morePosts, preview }: Props) {
         )}
       </Container>
     </Layout>
-  );
+  )
 }
 
 type Params = {
   params: {
-    slug: string;
-  };
-};
+    slug: string
+  }
+}
 
 export async function getStaticProps({ params }: Params) {
   const post = getPostBySlug(params.slug, [
@@ -67,8 +67,8 @@ export async function getStaticProps({ params }: Params) {
     "content",
     "ogImage",
     "coverImage",
-  ]);
-  const content = await markdownToHtml(post.content || "");
+  ])
+  const content = await markdownToHtml(post.content || "")
 
   return {
     props: {
@@ -77,11 +77,11 @@ export async function getStaticProps({ params }: Params) {
         content,
       },
     },
-  };
+  }
 }
 
 export async function getStaticPaths() {
-  const posts = getAllPosts(["slug"]);
+  const posts = getAllPosts(["slug"])
 
   return {
     paths: posts.map((post) => {
@@ -89,8 +89,8 @@ export async function getStaticPaths() {
         params: {
           slug: post.slug,
         },
-      };
+      }
     }),
     fallback: false,
-  };
+  }
 }
