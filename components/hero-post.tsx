4ba4adc3ec242ca31ce646595010cb7fd69c5367
Rob Lloveras <rob@lloveras.org>
2022-08-14 23:48:52 -0400

    trying refactor with HTML

--- components/hero-post.tsx
+++ components/hero-post.tsx
@@ -1,8 +1,8 @@
-import Avatar from './avatar'
-import DateFormatter from './date-formatter'
-import CoverImage from './cover-image'
-import Link from 'next/link'
-import type Author from '../interfaces/author'
+import Link from "next/link"
+import type Author from "../interfaces/author"
+import Avatar from "./avatar"
+import CoverImage from "./cover-image"
+import DateFormatter from "./date-formatter"
 
 type Props = {
   title: string
@@ -13,34 +13,27 @@ type Props = {
   slug: string
 }
 
-const HeroPost = ({
-  title,
-  coverImage,
-  date,
-  excerpt,
-  author,
-  slug,
-}: Props) => {
+const HeroPost = ({ title, coverImage, date, excerpt, author, slug }: Props) => {
   return (
     <section>
-      <div className="mb-8 md:mb-16">
-        <CoverImage title={title} src={coverImage} slug={slug} />
-      </div>
-      <div className="md:grid md:grid-cols-2 md:gap-x-16 lg:gap-x-8 mb-20 md:mb-28">
+      <div className="mb-15 md:flex md:justify-between md:gap-x-16 lg:gap-x-8 md:mb-10">
         <div>
-          <h3 className="mb-4 text-4xl lg:text-5xl leading-tight">
+          <h3 className="mb-4 text-4xl leading-tight lg:text-5xl">
             <Link as={`/posts/${slug}`} href="/posts/[slug]">
               <a className="hover:underline">{title}</a>
             </Link>
           </h3>
-          <div className="mb-4 md:mb-0 text-lg">
+          <div className="mb-4 text-lg md:mb-0">
             <DateFormatter dateString={date} />
           </div>
         </div>
-        <div>
-          <p className="text-lg leading-relaxed mb-4">{excerpt}</p>
+        <div className="mb-10 md:mb-0">
+          <p className="mb-5 text-lg leading-relaxed">{excerpt}</p>
           <Avatar name={author.name} picture={author.picture} />
         </div>
+      </div>
+      <div className="mb-8 md:mb-16">
+        <CoverImage title={title} src={coverImage} slug={slug} />
       </div>
     </section>
   )
