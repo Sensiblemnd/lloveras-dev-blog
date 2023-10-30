ab451562974cac89168f1270ddc0d6ae2f68aec6
Rob Lloveras <Sensiblemnd@users.noreply.github.com>
2023-10-29 20:23:48 -0400

    test

--- components/drawer-menu.tsx
+++ components/drawer-menu.tsx
@@ -20,7 +20,10 @@ export const DrawerMenu = () => {
         </Link>
       </li>
 
-      <li className="pt-2 pb-2  pl-4  text-xl border-white lg:pt-[revert] lg:pb-[revert] lg:pl-[revert] lg:text-[1rem]">
+      <li
+        style={{ padding: "100px" }}
+        className="pt-2 pb-2  pl-4  text-xl border-white lg:pt-[revert] lg:pb-[revert] lg:pl-[revert] lg:text-[1rem]"
+      >
         <Link href="/gooey-menu">
           <a className="text-shadow">Gooey Menu </a>
         </Link>
