005c084a9bd5610b5e9ed838ebf20485df57f4d7
Rob Lloveras <Sensiblemnd@users.noreply.github.com>
2023-10-29 20:23:12 -0400

    z

--- components/drawer-menu.tsx
+++ components/drawer-menu.tsx
@@ -14,23 +14,23 @@ const Li = ({ children }: Props) => {
 export const DrawerMenu = () => {
   return (
     <>
-      <Li>
+      <li className="pt-2 pb-2  pl-4  text-xl border-white lg:pt-[revert] lg:pb-[revert] lg:pl-[revert] lg:text-[1rem]">
         <Link href="/">
           <a className="text-shadow">Home</a>
         </Link>
-      </Li>
+      </li>
 
-      <Li>
+      <li className="pt-2 pb-2  pl-4  text-xl border-white lg:pt-[revert] lg:pb-[revert] lg:pl-[revert] lg:text-[1rem]">
         <Link href="/gooey-menu">
           <a className="text-shadow">Gooey Menu </a>
         </Link>
-      </Li>
+      </li>
 
-      <Li>
+      <li className="pt-2 pb-2  pl-4  text-xl border-white lg:pt-[revert] lg:pb-[revert] lg:pl-[revert] lg:text-[1rem]">
         <Link href="/codepen">
           <a className="text-shadow">Codepen Experiments</a>
         </Link>
-      </Li>
+      </li>
     </>
   )
 }
