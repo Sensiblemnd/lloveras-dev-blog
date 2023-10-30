87926c4b89f7a21c3296692d3f4a1d8357217295
Rob Lloveras <Sensiblemnd@users.noreply.github.com>
2023-10-29 20:27:19 -0400

    j

--- components/drawer-menu.tsx
+++ components/drawer-menu.tsx
@@ -1,15 +1,4 @@
 import Link from "next/link"
-type Props = {
-  children: React.ReactNode | string
-}
-
-const Li = ({ children }: Props) => {
-  return (
-    <li className="pt-2 pb-2  pl-4  text-xl border-white lg:pt-[revert] lg:pb-[revert] lg:pl-[revert] lg:text-[1rem]">
-      {children}
-    </li>
-  )
-}
 
 export const DrawerMenu = () => {
   return (
