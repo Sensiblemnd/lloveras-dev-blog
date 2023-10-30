e3a665e1770c31cfc517cc0d048dd73d195392bf
Rob Lloveras <Sensiblemnd@users.noreply.github.com>
2023-10-29 20:32:30 -0400

    fix

--- components/drawer-menu.tsx
+++ components/drawer-menu.tsx
@@ -3,22 +3,19 @@ import Link from "next/link"
 export const DrawerMenu = () => {
   return (
     <>
-      <li className="pt-2 pb-2  pl-4  text-xl border-white lg:pt-[revert] lg:pb-[revert] lg:pl-[revert] lg:text-[1rem]">
+      <li className="  text-xl lg:text-[1rem]">
         <Link href="/">
           <a className="text-shadow">Home</a>
         </Link>
       </li>
 
-      <li
-        style={{ padding: "100px" }}
-        className="pt-2 pb-2  pl-4  text-xl border-white lg:pt-[revert] lg:pb-[revert] lg:pl-[revert] lg:text-[1rem]"
-      >
+      <li className="  text-xl lg:text-[1rem]">
         <Link href="/gooey-menu">
           <a className="text-shadow">Gooey Menu </a>
         </Link>
       </li>
 
-      <li className="pt-2 pb-2  pl-4  text-xl border-white lg:pt-[revert] lg:pb-[revert] lg:pl-[revert] lg:text-[1rem]">
+      <li className="  text-xl lg:text-[1rem]">
         <Link href="/codepen">
           <a className="text-shadow">Codepen Experiments</a>
         </Link>
